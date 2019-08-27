5667ef97fddd2f5e5a0ccab901b6e2462436b715
boody elgml <booodyelgml@gmail.com>
2019-08-28 02:08:14 +0300

    solve hover issues

--- js/script.js
+++ js/script.js
@@ -5,4 +5,23 @@ $(function () {
     });
     
 });
+//count js
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
 
+
+
+// Dependency: CountUp.js: https://github.com/inorganik/CountUp.js
+
+$(window).on("load" , function(){
+    $(".loader").fadeOut()
+})
